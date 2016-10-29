d260fec74fd45a04be9d856506d47c59b1cf99e4
tiddi2 <tidemann.tiddi@hotmail.com>
2016-10-29 20:46:28 +0200

    lagt til save knapp og gjort klar for å skrive funksjon sjekkData

--- Firebase/public/map/setupProfile.js
+++ Firebase/public/map/setupProfile.js
@@ -1,9 +1,12 @@
 var currentTime = new Date();
 var currentYear = currentTime.getFullYear();
-var currentMonthZeroIndex = currentTime.getMonth();
-var currentMonthOneIndex = currentMonthZeroIndex += 1;
-var currentDate = currentTime.getDate();
-const monthName = ["Januar", "Februar", "Mars", "April", "Mail", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"];
+const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const bornYear = document.getElementById("bornYear");
+const bornMonth = document.getElementById("bornMonth");
+const bornDate = document.getElementById("bornDate");
+const savetoDatabase = document.getElementById("savetoDatabase")
+savetoDatabase.onclick = sjekkData
+bornMonth.onchange = checkDatesInMonth;
 
 //liste med land for nedtrekkslista
 const country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre and Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts and Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
@@ -24,19 +27,36 @@ for (var i = 0; i <= 100; i++) {
     const aarOption = document.createElement('option');
     aarOption.text = currentYear - i;
     aarOption.value = currentYear - i;
-    document.getElementById("fodselsAar").appendChild(aarOption);
+    bornYear.appendChild(aarOption);
     //Sjekker hvor mange dager det er i måneden
-    if (i > 0 && i <= daysInMonth(currentMonthOneIndex, currentYear)) {
+    if (i > 0 && i <= 31) {
         const dagOption = document.createElement('option');
         dagOption.text = i;
         dagOption.value = i;
-        document.getElementById("fodselsDag").appendChild(dagOption);
+        bornDate.appendChild(dagOption);
     }
     if (i < 12) {
         const maanedrOption = document.createElement('option');
         maanedrOption.text = monthName[i];
         maanedrOption.value = i;
-        document.getElementById("fodselsMaaned").appendChild(maanedrOption);
+        bornMonth.appendChild(maanedrOption);
+    }
+}
+
+function sjekkData() {
+    // TODO: Sjekk for gyldig data
+    //Skriv så til database
+
+}
+function checkDatesInMonth() {
+    var selectedYear = parseInt(bornYear.options[bornYear.selectedIndex].value);
+    var selectedMonth = parseInt(bornMonth.options[bornMonth.selectedIndex].value)+1;
+    var selectedDate = parseInt(bornDate.options[bornDate.selectedIndex].value);
+
+    console.log(new Date (selectedMonth + "/" + selectedDate + "/" + selectedYear))
+    //console.log(daysInMonth((selectedMonth+1), selectedYear))
+    while (bornDate.childNodes.length > daysInMonth(selectedMonth, selectedYear)) {
+        bornDate.removeChild(bornDate.firstChild);
     }
 }
 

5db1b0f16d4e00578b9ce8e3ba90e086d1769a89
Jim-Alexander Berger Gundersen <alexandergundersen@yahoo.no>
2016-10-29 21:20:27 +0200

    henter fra databasen og legger inn i nedtrekksliste

--- Firebase/public/map/databaseTesting/database.js
+++ Firebase/public/map/databaseTesting/database.js
@@ -14,8 +14,15 @@ function addRoute(){
 addToDatabase(navn,navn,start,slutt,omvei,tid);
 }
 
-ruter.on("value", function(snapshot){
-  output.innerHTML = snapshot.child("stopPlace").val();
+ruter.on("child_added", function(snapshot){
+ for (user in snapshot.val()){
+   for(rute in snapshot.child(user).val()){
+     let start = snapshot.child(user).child(rute).val().startPlace;
+     let stop = snapshot.child(user).child(rute).val().stopPlace;
+     let id = rute;
+     output.innerHTML += "<option id='" +  id + "'>" + start + " - " + stop + "</option>";
+   }
+ }
 })
 
 function addToDatabase(id,navn,start,slutt,omvei,tid){

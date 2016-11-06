232eed8b10e365b739c7c5bcc9a39f35e73e722b
tiddi2 <tidemann.tiddi@hotmail.com>
2016-11-06 05:36:29 +0100

    Endret hele hovedsiden

--- Firebase/public/js/loginscript.js
+++ Firebase/public/js/loginscript.js
@@ -10,46 +10,11 @@ var config = {
 };
 firebase.initializeApp(config);
 
-//konstanter for html elementene
-const btnLogin = document.getElementById("btnLogin");
-//const btnSignout = document.getElementById("btnSignout");
-
-
-btnLogin.addEventListener("click", e => {
-    firebase.auth().signInWithRedirect(provider);
-    firebase.auth().getRedirectResult().then(function(result) {
-        if (result.credential) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            // ...
-        }
-        // The signed-in user info.
-        var user = result.user;
-    }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-    });
-});
-
 firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
-        var userId = firebase.auth().currentUser.uid;
-        firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
-            if(snapshot.val()) {
-                window.location.href = "/map/index.html"
-            }
-            else {
-                window.location.href = "/map/setupProfile.html"
-            }
-        });
+        console.log("inlogget")
     }
     else {
-        console.log("not logged in")
+        firebase.auth().signInWithRedirect(provider);
     }
 });
--- Firebase/public/js/main.js
+++ Firebase/public/js/main.js
@@ -1,122 +1,55 @@
-/* ========================================================================= */
-/*  Preloader Script
-/* =========================================================================
+jQuery(function($) {
 
-window.onload = function () {
-    document.getElementById('loading-mask').style.display = 'none';
-} */
+	$(function(){
+		$('#main-slider.carousel').carousel({
+			interval: 10000,
+			pause: false
+		});
+	});
 
-$(function(){
-    /* ========================================================================= */
-    /*  Menu item highlighting
-    /* ========================================================================= */
+	//Ajax contact
+	var form = $('.contact-form');
+	form.submit(function () {
+		$this = $(this);
+		$.post($(this).attr('action'), function(data) {
+			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+		},'json');
+		return false;
+	});
 
-    jQuery(window).scroll(function () {
-        if (jQuery(window).scrollTop() > 400) {
-            jQuery("#navigation").css("background-color","#00C7FC");
-            jQuery("#navigation").addClass("animated-nav");
-        } else {
-            jQuery("#navigation").css("background-color","transparent");
-            jQuery("#navigation").removeClass("animated-nav");
-        }
-    });
+	//smooth scroll
+	$('.navbar-nav > li').click(function(event) {
+		event.preventDefault();
+		var target = $(this).find('>a').prop('hash');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		}, 500);
+	});
 
-    $('#nav').onePageNav({
-        filter: ':not(.external)',
-        scrollSpeed: 950,
-        scrollThreshold: 1
-    });
+	//scrollspy
+	$('[data-spy="scroll"]').each(function () {
+		var $spy = $(this).scrollspy('refresh')
+	})
 
-    // Slider Height
-    var slideHeight = $(window).height();
-    $('#home-carousel .carousel-inner .item, #home-carousel .video-container').css('height',slideHeight);
+	//PrettyPhoto
+	$("a.preview").prettyPhoto({
+		social_tools: false
+	});
 
-    $(window).resize(function(){'use strict',
-        $('#home-carousel .carousel-inner .item, #home-carousel .video-container').css('height',slideHeight);
-    });
-
-    // portfolio filtering
-
-    $("#projects").mixItUp();
-
-    //fancybox
-
-    $(".fancybox").fancybox({
-        padding: 0,
-
-        openEffect : 'elastic',
-        openSpeed  : 650,
-
-        closeEffect : 'elastic',
-        closeSpeed  : 550,
-    });
-
-
-    /* ========================================================================= */
-    /*  Facts count
-    /* ========================================================================= */
-
-    "use strict";
-    $(".fact-item").appear(function () {
-        $(".fact-item [data-to]").each(function () {
-            var e = $(this).attr("data-to");
-            $(this).delay(6e3).countTo({
-                from: 50,
-                to: e,
-                speed: 3e3,
-                refreshInterval: 50
-            })
-        })
-    });
-
-/* ========================================================================= */
-/*  On scroll fade/bounce fffect
-/* ========================================================================= */
-
-    $("#testimonial").owlCarousel({
-        pagination : true, // Show bullet pagination
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-    });
-
+	//Isotope
+	$(window).load(function(){
+		$portfolio = $('.portfolio-items');
+		$portfolio.isotope({
+			itemSelector : 'li',
+			layoutMode : 'fitRows'
+		});
+		$portfolio_selectors = $('.portfolio-filter >li>a');
+		$portfolio_selectors.on('click', function(){
+			$portfolio_selectors.removeClass('active');
+			$(this).addClass('active');
+			var selector = $(this).attr('data-filter');
+			$portfolio.isotope({ filter: selector });
+			return false;
+		});
+	});
 });
-
-/* ========================================================================= */
-/*  On scroll fade/bounce fffect
-/* ========================================================================= */
-
-    wow = new WOW({
-        animateClass: 'animated',
-        offset: 100,
-        mobile: false
-    });
-    wow.init();
-
-/* ---------------------------------------------------------------------- */
-/*      Progress Bars
-/* ---------------------------------------------------------------------- */
-
-initProgress('.progress');
-
-function initProgress(el){
-    jQuery(el).each(function(){
-        var pData = jQuery(this).data('progress');
-        progress(pData,jQuery(this));
-    });
-}
-
-
-            
-function progress(percent, $element) {
-    var progressBarWidth = 0;
-    
-    (function myLoop (i,max) {
-        progressBarWidth = i * $element.width() / 100;
-        setTimeout(function () {   
-        $element.find('div').find('small').html(i+'%');
-        $element.find('div').width(progressBarWidth);
-        if (++i<=max) myLoop(i,max);     
-        }, 10)
-    })(0,percent);  
-}   
